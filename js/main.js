646e8b91e44bad47261be409dc1536fa418626d7
meso1991 <dstehr12@asu.edu>
2026-09-02 02:00:20 +0300

    Add Meta Pixel tracking

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 const REDSTAR_WHATSAPP = "201555988432";
 const REVIEW_STORAGE_KEY = "redstar_reviews";
 
+(function () {
+    if (window.fbq) return;
+
+    !function (f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function () {
+            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    if (window.fbq) {
+        window.fbq('init', '1413400023981892');
+        window.fbq('track', 'PageView');
+    }
+
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1413400023981892&ev=PageView&noscript=1" />';
+    document.head.appendChild(noscript);
+})();
+
 // قاموس الترجمة المعتمد لجميع صفحات الموقع
 const translations = {
     ar: {
